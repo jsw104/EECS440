18582e62d06dc68f665d450e6e5b5f877247a6b5
Justin Wang <justin@MacBookPro-5C969D721D17-2.local>
2017-09-28 18:55:54 -0400

    more processing and evaluation stuff

--- problem2/neuralNetwork.py
+++ problem2/neuralNetwork.py
@@ -13,8 +13,49 @@ class NeuralNetwork:
         self.schema = exampleSet.schema
         self.useCrossValidation = useCrossValidation
         self.hiddenLayerNodes = []
-        for i in range(1, numberOfHiddenNodes):
-            self.hiddenLayerNodes.append(NeuralNode(len(self.schema.features)))
-        self.outputNode = NeuralNode(len(self.hiddenLayerNodes))
         self.weightDecay = weightDecay
         self.numberOfTrainingIterations = numberOfTrainingIterations
+
+        self.nominalAttributeHash = {}
+        self.constructNominalAttributeHash()
+
+        for i in range(0, numberOfHiddenNodes):
+            #the first and last feature are not useful
+            self.hiddenLayerNodes.append(NeuralNode(len(self.schema.features) - 2))
+        self.outputNode = NeuralNode(len(self.hiddenLayerNodes))
+
+        for i in range (0, numberOfTrainingIterations):
+            print("Currently on " + str(i) + " training iteration")
+            self.executeTrainingIteration(exampleSet.examples)
+
+    def constructNominalAttributeHash(self):
+        for i in range(1, len(self.schema) - 1):
+            if self.schema[i].type == 'NOMINAL':
+                for value in self.schema[i].values:
+                    if value not in self.nominalAttributeHash:
+                        self.nominalAttributeHash[value] = len(self.nominalAttributeHash.keys())
+
+    def executeTrainingIteration(self, trainingExamples):
+        for example in trainingExamples:
+            example = self.normalizeExample(example)
+            self.evaluateExample(example)
+
+    def normalizeExample(self, example):
+        inputList = []
+        for i in range(1, len(example) - 1):
+            inputList.append(self.normalizeFeature(example[i])) if (self.schema[i].type == 'NOMINAL') else inputList.append(example[i])
+        return inputList
+
+    def normalizeFeature(self, feature):
+        if feature in self.nominalAttributeHash:
+            return self.nominalAttributeHash[feature]
+        return feature
+
+    def evaluateExample(self, example):
+        hiddenLayerOutputs = []
+        for hiddenLayerNode in self.hiddenLayerNodes:
+            summation = hiddenLayerNode.calculateInputWeightSummation(example)
+            hiddenLayerOutputs.append(hiddenLayerNode.calculateSigmoid(summation))
+        outputNodeSummation = self.outputNode.calculateInputWeightSummation(hiddenLayerOutputs)
+        output = self.outputNode.calculateSigmoid(outputNodeSummation)
+        #calculate error from expected output here
--- problem2/neuralNode.py
+++ problem2/neuralNode.py
@@ -1,7 +1,24 @@
 import random
+import math
+
 class NeuralNode:
     def __init__(self, numberOfWeights):
         self.weights = []
-        for i in range(1, numberOfWeights):
+        for i in range(0, numberOfWeights):
             self.weights.append(random.uniform(0, 1))
 
+    def calculateInputWeightSummation(self, inputs):
+        if (len(inputs) != len(self.weights)):
+            print "We have a problem"
+        sum = 0
+        for i in range(0, len(self.weights)):
+            sum = sum + inputs[i] * self.weights[i]
+        return sum
+
+    #use this for evaluation phase.
+    def willFire(self, summation):
+        return (self.calculateSigmoid(summation) > 0.5)
+
+    #use this for training phase
+    def calculateSigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
