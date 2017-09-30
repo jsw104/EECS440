Fix some backpropagation bugs

problem2/neuralNetwork.py
@@ -35,23 +35,17 @@ class NeuralNetwork:
 
         self.nominalAttributeHash = {}
         self._constructNominalAttributeHash()
-
-        #for i in range(1, numberOfHiddenNodes):
-            #the first and last feature are not useful
-            #self.hiddenLayerNodes.append(NeuralNode(len(self.schema.features) - 2))
-        
-        #self.outputNode = NeuralNode(len(self.hiddenLayerNodes))
         
         self.evaluateNetworkPerformance(self.examples)
         for i in range (0, numberOfTrainingIterations):
             print("Currently on training iteration " + str(i+1))
             self.executeTrainingIteration(self.examples)
             self.evaluateNetworkPerformance(self.examples)
-            for layer in self.layers:
-                print 'biases'
-                print layer.biases
-                print 'weights'
-                print layer.weights
+            #for layer in self.layers:
+            #    print 'biases'
+            #    print layer.biases
+            #    print 'weights'
+            #    print layer.weights
                 
 
     def _constructNominalAttributeHash(self):
@@ -66,7 +60,7 @@ class NeuralNetwork:
         layerInputs = []
         layerActivationDerivs = []
         layerInputs.append(inputs)
-        layerActivationDerivs.append(None) #Dummy entry to keep indexes aligned
+        #layerActivationDerivs.append(None) #Dummy entry to keep indexes aligned
         while True:
             layerOutputs, activationDerivs = self.layers[layerIndex].getOutputs(layerInputs[-1])
             layerInputs.append(layerOutputs)
@@ -81,16 +75,9 @@ class NeuralNetwork:
         downstreamWeights = np.ones(len(errors)) #might need to transpose this if we have multiple outputs
         
         while layerIndex >= 0:
-            print '============================================'
             layer = self.layers[layerIndex]
-            print layer.numInputs, layer.numNodesThisLayer
-            print layer.biases
-            print layer.weights
-            #print 'LAYER:' + str(layerIndex) + ' nodes: ' + str(layer.numNodesThisLayer) + ' inputs: ' + str(layer.numInputs)
-            downstreamWeights, downstreamBiasSensitivities = layer.backpropagate(layerInputs[layerIndex+1],layerActivationDerivs[layerIndex+1],downstreamBiasSensitivities,downstreamWeights)
+            downstreamWeights, downstreamBiasSensitivities = layer.backpropagate(layerInputs[layerIndex],layerActivationDerivs[layerIndex],downstreamBiasSensitivities,downstreamWeights)
             layerIndex = layerIndex - 1
-            print '============================================'
-
     
     def executeTrainingIteration(self, trainingExamples):
         for example in trainingExamples:
@@ -100,9 +87,7 @@ class NeuralNetwork:
             inputs = self.normalizeExample(example)
             targets = np.array([1.0]) if example[-1] else np.array([0.0])
             self._train(inputs, targets)      
-            break #for debug only -- remove this
-        
-        
+            break #for debug only -- remove this     
 
     def normalizeExample(self, example):
         inputList = []
@@ -122,7 +107,7 @@ class NeuralNetwork:
         values = exampleInputs
         for layer in self.layers:
             values, derivs = layer.getOutputs(values)
-            
+                    
         errors = targetOutputs - values
         return errors
 

problem2/neuralNetworkLayer.py
@@ -6,19 +6,14 @@ class NeuralNetworkLayer:
 	def __init__(self, numNodes, numInputs):
 		self.numNodesThisLayer = numNodes
 		self.numInputs = numInputs	
-		#self.biases = np.random.uniform(-0.1,0.1,size=(numNodes))
-		#self.weights = np.random.uniform(-0.1,0.1,size=(numNodes,numInputs))
-		self.biases = np.zeros(shape=(numNodes))
-		self.weights = np.zeros(shape=(numNodes,numInputs))
+		self.biases = np.random.uniform(-0.1,0.1,size=(numNodes,1))
+		self.weights = np.random.uniform(-0.1,0.1,size=(numNodes,numInputs))
+		#self.biases = np.zeros(shape=(numNodes,1))
+		#self.weights = np.zeros(shape=(numNodes,numInputs))
 		
 	def calculateInputWeightSummations(self, layerInputs):
-		#print np.array(layerInputs)
-		#print self.weights.transpose() 
-		#print self.biases
-		#print np.array(layerInputs).dot(self.weights.transpose()) + self.biases
-		#print np.transpose(np.array(layerInputs).dot(self.weights.transpose()) + self.biases)
-		#raise RuntimeError
-		return np.array(layerInputs).dot(self.weights.transpose()) + self.biases
+		layerInputs = np.reshape(layerInputs, (1,-1))
+		return np.reshape(layerInputs.dot(self.weights.transpose()),(-1,1)) + self.biases
 	
 	#use this for evaluation phase.
 	def willFire(self, weightedSums):
@@ -27,8 +22,9 @@ class NeuralNetworkLayer:
 		 
 	def applyActivationFunction(self, weightedSums):
 		#print weightedSums
-		outputs = np.copy(np.array(weightedSums))
+		outputs = np.copy(weightedSums)
 		derivsActivationFunc = np.copy(outputs)	
+		#print outputs
 		for i in range(0, len(weightedSums)):
 			outputs[i] = 1.0 / (1.0 + math.exp(-1.0*weightedSums[i]))
 			derivsActivationFunc[i] = outputs[i] * (1.0-outputs[i])
@@ -54,26 +50,19 @@ class NeuralNetworkLayer:
 	#	return oldWeights, biasSensitivities, weightSensitivities
 		
 	def backpropagate(self, layerInputs, derivs, downstreamBiasSensitivities, downstreamWeights, learningRate = 0.01):
-		print 'layerInputs: ' + str(layerInputs)
-		print 'derivs: ' + str(derivs)
-		print 'downstreamBiasSensitivities: ' + str(downstreamBiasSensitivities)
-		print 'downstreamWeights: ' + str(downstreamWeights)
-		
-		biasSensitivities = np.multiply(downstreamWeights.transpose().dot(downstreamBiasSensitivities), derivs)
-		
-		print 'biasSensitivities: ' + str(biasSensitivities)
-		
-		weightSensitivities = np.copy(self.weights)
-		
-		for row in weightSensitivities:
-			for i in range(0, len(row)-1):
-				row[i] = biasSensitivities[i]
+		#print 'layerInputs: ' + str(layerInputs)
+		derivs = np.reshape(derivs,(-1,1))
+		#print 'derivs: ' + str(derivs)
+		#print 'downstreamBiasSensitivities: ' + str(downstreamBiasSensitivities)
+		#print 'downstreamWeights: ' + str(downstreamWeights)
+		#print 'lefthalfofmultiply: ' + str(downstreamWeights.transpose().dot(np.reshape(downstreamBiasSensitivities,(1,-1))))
+		biasSensitivities = np.multiply(downstreamWeights.transpose().dot(np.reshape(downstreamBiasSensitivities,(1,-1))), derivs)
 				
+		#print 'biasSensitivities: ' + str(biasSensitivities)
 				
+		weightSensitivities = biasSensitivities.dot(np.reshape(layerInputs,(1,-1)))
 		
-		weightSensitivities = biasSensitivities.dot(layerInputs)
-		
-		print 'weightSensitivities: ' + str(weightSensitivities)
+		#print 'weightSensitivities: ' + str(weightSensitivities)
 		
 		oldBiases = np.copy(self.biases)
 		oldWeights = np.copy(self.weights)
@@ -81,10 +70,14 @@ class NeuralNetworkLayer:
 		self.biases = self.biases - (learningRate * biasSensitivities)
 		self.weights = self.weights - (learningRate * weightSensitivities)
 		
-		#print 'oldBiases: ' + str(oldBiases)
-		#print 'biases: ' + str(self.biases)
-		#print 'oldWeights: ' + str(oldWeights)
-		#print 'weights: ' + str(self.weights)
+		#print '======= oldBiases ======='
+		#print oldBiases
+		#print '======= biases =======' 
+		#print self.biases
+		#print '======= oldWeights ======='
+		#print oldWeights
+		#print '======= weights =======' 
+		#print self.weights
 		
 		return oldWeights, biasSensitivities
 		